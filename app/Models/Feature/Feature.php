1c805a119ac12306a239b83dd58eb2666028ab00
ashtr3 <ashley@ashtree.studio>
2024-12-05 22:19:42 -0500

    fixed exclude overridden filter

--- app/Models/Feature/Feature.php
+++ app/Models/Feature/Feature.php
@@ -236,7 +236,7 @@ class Feature extends Model {
         $ids = $query->pluck('features.id')->toArray();
 
         return $query->whereDoesntHave('hiddenBy', function ($query) use ($ids) {
-            $query->whereIn('features.id', $ids);
+            $query->whereIn('override_id', $ids);
         });
     }
 
